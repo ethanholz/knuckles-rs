a91c97b267a287a050d10f61429432546ca94f75
Ethan Holz <ethan.holz@thoriumworks.com>
2024-11-13 11:15:16 -0700

    test(term): add term tests

--- knuckles-parse/src/records/term.rs
+++ knuckles-parse/src/records/term.rs
@@ -26,7 +26,10 @@ impl TermRecord {
             res_name: str[17..20].trim().to_string(),
             chain_id: str[21..22].parse().unwrap_or_default(),
             res_seq: str[22..26].trim().parse().unwrap_or_default(),
-            i_code: str.get(26..27).map(|s| s.parse().unwrap_or_default()),
+            i_code: str
+                .get(26..27)
+                .map(|s| s.parse().unwrap_or_default())
+                .filter(|item| *item != ' '),
         }
     }
 }
@@ -34,5 +37,34 @@ impl TermRecord {
 impl From<&str> for TermRecord {
     fn from(str: &str) -> Self {
         TermRecord::new(str)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let str =
+            "TER     297      ALA A  18                                                      ";
+        let record = TermRecord::new(str);
+        assert_eq!(record.serial, 297);
+        assert_eq!(record.res_name, "ALA");
+        assert_eq!(record.chain_id, 'A');
+        assert_eq!(record.res_seq, 18);
+        assert_eq!(record.i_code, None);
+    }
+
+    #[test]
+    fn test_from() {
+        let str =
+            "TER     297      ALA A  18                                                      ";
+        let record = TermRecord::from(str);
+        assert_eq!(record.serial, 297);
+        assert_eq!(record.res_name, "ALA");
+        assert_eq!(record.chain_id, 'A');
+        assert_eq!(record.res_seq, 18);
+        assert_eq!(record.i_code, None);
     }
 }
